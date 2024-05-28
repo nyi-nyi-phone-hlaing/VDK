4eaa66eedd3c6ddd09d198aef875d458bdbb003b
Nyi Nyi Phone Hlaing <nnph123725@gmail.com>
2024-05-28 12:42:14 +0630

    with Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1>Hello Vite</h1>
+      <h1 className='text-xl font-bold text-blue-700 text-center'>
+        Hello Vite + React with Tailwind CSS
+      </h1>
     </>
   );
 }
