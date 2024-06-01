92acada0368c472d4ae6cb66b0937be165bec040
Nyi Nyi Phone Hlaing <nnph123725@gmail.com>
2024-06-01 14:11:11 +0630

    Added react-router-dom package

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <h1 className='text-xl font-bold text-blue-700 text-center'>
-        Hello Vite + React with Tailwind CSS
+        Hello Vite + React with React Router Dom
       </h1>
     </>
   );
