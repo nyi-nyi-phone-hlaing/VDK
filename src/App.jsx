3e28f77e8f46c72cfde8a2fdcb03856261f7f5d9
webWizard08 <webWizard123725@gmail.com>
2024-07-07 21:10:44 +0630

    Updated Project Folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 function App() {
   return (
-    <>
-      <h1 className='text-xl font-bold text-blue-700 text-center'>
-        Hello Vite + React with React Router Dom
+    <div className='font-poppins w-full h-screen flex flex-col justify-center items-center bg-blue-50'>
+      <h1 className=' text-3xl font-extrabold text-blue-500 text-center'>
+        😍 Hello World 😍
       </h1>
-    </>
+      <p className='text-zinc-500 text-sm font-semibold'>Coding is awesome</p>
+      <ul className='text-blue-600 text-center font-semibold'>
+        <li>React + Vite</li>
+        <li>Tailwind Css</li>
+        <li>React Router Dom</li>
+      </ul>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
